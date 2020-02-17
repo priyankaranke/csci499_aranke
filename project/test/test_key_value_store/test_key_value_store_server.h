3200ac6e50d960b09bc551af8d5a2855fc6cceb5
Priyank Aranke <aranke@usc.edu>
2020-02-17 15:04:59 -0800

    fixed changes suggested

--- project/test/test_key_value_store/test_key_value_store_server.h
+++ project/test/test_key_value_store/test_key_value_store_server.h
@@ -2,22 +2,20 @@
 
 // The fixture for testing class Foo.
 class KeyValueStoreServerTest : public ::testing::Test {
+ protected:
+   KeyValueStoreServerTest();
 
-protected:
+   // You can do clean-up work that doesn't throw exceptions here.
+   virtual ~KeyValueStoreServerTest();
 
-    KeyValueStoreServerTest();
+   // If the constructor and destructor are not enough for setting up 
+   // and cleaning up each test, you can define the following methods:
 
-    // You can do clean-up work that doesn't throw exceptions here.
-    virtual ~KeyValueStoreServerTest();
+   // Code here will be called immediately after the constructor (right
+   // before each test).
+   virtual void SetUp();
 
-    // If the constructor and destructor are not enough for setting up
-    // and cleaning up each test, you can define the following methods:
-
-    // Code here will be called immediately after the constructor (right
-    // before each test).
-    virtual void SetUp();
-
-    // Code here will be called immediately after each test (right
-    // before the destructor).
-    virtual void TearDown();
+   // Code here will be called immediately after each test (right
+   // before the destructor).
+   virtual void TearDown();
 };
