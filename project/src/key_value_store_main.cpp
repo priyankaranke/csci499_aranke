7d71d607612501e91eab313c829e035adbd8ba9f
Priyank Aranke <aranke@usc.edu>
2020-02-20 12:12:36 -0800

    added missing files

---
+++ project/src/key_value_store_main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "key_value_store_server.h"
+
+void RunServer() {
+  std::string server_address("0.0.0.0:50001");
+  KeyValueStoreServer service;
+
+  ServerBuilder builder;
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  builder.RegisterService(&service);
+  std::unique_ptr<Server> server(builder.BuildAndStart());
+  std::cout << "Key value Store Server listening on " << server_address << std::endl;
+  server->Wait();
+}
+
+int main(int argc, char** argv) {
+  // Initialize Google's logging library.
+  google::InitGoogleLogging(argv[0]);
+  LOG(INFO) << "First GLOG message " << std::endl;
+  RunServer();
+  return 0;
+}
